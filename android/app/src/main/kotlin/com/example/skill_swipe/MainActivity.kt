cabac484f5ed9eacc5e488dda66aba069f147d0b
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-09-29 17:26:06 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/skill_swipe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.skill_swipe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:skill_swipe/pages/intro_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Skill Swipe',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const IntroPage(),
+    );
+  }
+}
